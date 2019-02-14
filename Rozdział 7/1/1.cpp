3b5ad5ab6fe92ddbd2e5530460dae4a24c72ba2e
Julia <julcia106@onet.eu>
2019-02-14 23:19:35 +0100

    correction

--- Rozdział 7/1/1.cpp
+++ Rozdział 7/1/1.cpp
@@ -1,23 +1,25 @@
 ﻿#include "pch.h"
 #include <iostream>
 
-using namespace std;
-
-void harmoniczna (int x, int y);
+double harmoniczna (int x, int y);
 
 int main () {
 	int a, b;
+	std::cout << "Podaj dwie liczby, 0 konczy\n";
+	std::cin >> a >> b; 
 
-	while ( a != 0 && b != 0 ) {
-		cin >> a >> b; //TODO
-		harmoniczna (a, b);
+	while ( a!=0 && b!=0 ) {
+
+		std::cout << "Srednia harmoniczna: " << harmoniczna (a, b) << std::endl;
+		std::cout << "Podaj dwie kolejne liczby: \n";
+		std::cin >> a >> b;
+
+
 	}
 }
-
-void harmoniczna (int x, int y) {
+double harmoniczna (int x, int y) {
 	double srednia;
 
 	srednia = ((2.0 * x * y) / (x + y));
-	cout << srednia << endl;
-
+	return srednia;
 }
