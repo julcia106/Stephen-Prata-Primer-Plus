e0ea5cb54bc9291a59f53e16907919a10866fac1
Julia <julcia106@onet.eu>
2019-03-31 21:16:32 +0200

    STL

---
+++ Rozdział 16/1/1.cpp
@@ -0,0 +1,35 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool palindrom (string & str);
+
+int main () {
+
+	string word;
+
+	cout << "Prosze podac slowo: " << endl;
+	getline (cin, word);
+	cout << "Oto Twoje slowo: " << word << endl;
+
+	if ( palindrom (word) )
+		cout << "Twoje slowo jest palindromem. " << endl;
+	else
+		cout << "Twoje slowo nie jest palindromem. " << endl;
+
+}
+ 
+bool palindrom (string & str) {
+
+	string temp = str;
+
+	reverse (str.begin (), str.end ());
+
+	if ( temp.string::compare (str) == 0 )
+		return true;
+	else
+		return false;
+
+}
