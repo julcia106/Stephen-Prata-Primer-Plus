5c9576dd5ecf11b6ebd77ac80005f9bc2e53ff7d
Julia <julcia106@onet.eu>
2019-04-09 17:23:00 +0200

    Rozdział 16

---
+++ Rozdział 16/5/5.cpp
@@ -0,0 +1,40 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+const int NUM = 15;
+
+template <class T>
+int reduce (T ar[], int n);
+
+template <class T>
+void show (T ar[], int n);
+
+int main () {
+
+	long arr [NUM] = { 4,2,6,10,15,22,9,90,1,22,67, 4, 2, 76, 1 };
+
+	int temp = reduce(arr, NUM);
+
+	show(arr, temp);
+}
+
+template <class T>
+int reduce (T ar[], int n) {
+
+	std::sort (ar, ar + NUM);
+	T *pt = std::unique (ar, ar + NUM);
+
+	return pt - ar;
+}
+
+template <class T>
+void show (T ar[], int n) {
+
+	cout << "Lista: ";
+	for ( int i = 0; i < n; i++ ) {
+		cout << ar [i] << ", ";
+	}
+}
