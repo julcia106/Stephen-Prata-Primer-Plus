ecc20e3b9507ed6b3dedb52f1cdc4fcae70f5c6f
Julia <julcia106@onet.eu>
2019-04-11 21:24:19 +0200

    Rozdział 16

--- Rozdział 16/6/bank.cpp
+++ Rozdział 16/6/bank.cpp
@@ -3,6 +3,11 @@
 #include <iostream>
 #include "bank.h"
 
+// metody klasy Customer
+
+// kiedy klient trafia do kolejki, zapamiêtywany jest bie¿¹cy
+// czas systemowy, losowany jest te¿ czas obs³ugi klienta
+// (z zakresu od 1 do 3)
 void Customer::set (long when) {
 	processtime = std::rand () % 3 + 1;
 	arrive = when;
