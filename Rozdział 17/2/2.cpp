b77a9213be81dd9ee9c2533614e697b12add09d6
Julia <julcia106@onet.eu>
2019-04-30 22:53:35 +0200

    std::fstream

---
+++ Rozdział 17/2/2.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cstdlib>
+
+int main () {
+	
+	std::cout << "Podaj nazwe pliku: \n";
+	std::string plikName;
+	std::cin >> plikName;
+
+	std::fstream plik;
+	plik.open (plikName.c_str(),std::ios::out | std::ios::app); // out -> tworzy nowy plik | app-> dopisuje do pliku dane | in -> wprowadź do programu(odczyt)
+
+	if (!plik.is_open()) {	
+		std::cerr << "Nie mozna otworzyc pliku " << plikName << " do zapisu.\n";
+		exit (EXIT_FAILURE);
+	}
+
+	std::cout << "Prosze podac dane (znak '$' konczy): \n";
+	std::string dane;
+	while ( std::getline (std::cin, dane) && dane != "$" )
+		plik << dane;
+	
+	plik.close ();
+
+	return 0;
+}
