9d3cd0b5d3f06305d9e62f4d6b9f84703604162d
Julia <julcia106@onet.eu>
2019-05-04 20:32:33 +0200

    pointers and txt

--- Rozdział 17/6/Source.cpp
+++ Rozdział 17/6/Source.cpp
@@ -5,31 +5,29 @@
 #include <cstdlib>
 #include <string>
 
-using namespace std;
 
 const char* file = "emp.txt";
-
 int main (void) {
 
 	const int MAX = 2;
 	char ch;
 
 	//poka¿ pocz¹tkow¹ zawartoœæ
-	ifstream fin;
+	std::ifstream fin;
 	fin.open (file);
 
 	if ( fin.is_open () ) {
-		cout << "Oto aktualna zawartosc pliku "
+		std::cout << "Oto aktualna zawartosc pliku "
 			<< file << ":\n";
 		while ( fin.get (ch) )
-			cout << ch;
+			std::cout << ch;
 		fin.close ();
 	}
 
 	//dodaj nowe osoby
-	ofstream fout (file, ios_base::out | ios_base::app);
+	std::ofstream fout (file, std::ios_base::out | std::ios_base::app);
 	if ( !fout.is_open () ) {
-		cerr << "Nie mozna otworzyc pliku " << file << " do zapisu.\n";
+		std::cerr << "Nie mozna otworzyc pliku " << file << " do zapisu.\n";
 		exit (EXIT_FAILURE);
 	}
 
@@ -38,15 +36,15 @@ int main (void) {
 	int ct;
 	for ( ct = 0; ct < MAX; ++ct ) { 
 		char choice;
-		cout << "Prosze wybrac obiekt do utworzenia: \n"
+		std::cout << "Prosze wybrac obiekt do utworzenia: \n"
 			<< "e: employee  m: manager \n"
 			<< "f: fink      h: highfink\n"
 			<< "w: wyjscie\n";
-		cin >> choice;
+		std::cin >> choice;
 
 		while ( strchr ("emfhw", choice) == NULL ) {
-				cout << "Wpisz e,m,f,h lub w: ";
-				cin >> choice;
+				std::cout << "Wpisz e,m,f,h lub w: ";
+				std::cin >> choice;
 			}
 		if ( choice == 'w' )
 			break;
@@ -60,14 +58,14 @@ int main (void) {
 			case 'h': pc [ct] = new highfink;
 				break;
 		}
-		cin.get ();
+		std::cin.get ();
 		pc [ct]->SetAll ();
 		pc [ct]->writeall (fout);
 	}
 	int i;
-	cout << "\nLista pracownikow:\n";
+	std::cout << "\nLista pracownikow:\n";
 	for ( i = 0; i < ct; ++i ) {
-		cout << endl;
+		std::cout << std::endl;
 		pc [i]->ShowAll ();
 	}
 	for ( i = 0; i < ct; ++i ) 
@@ -77,23 +75,14 @@ int main (void) {
 	fin.clear ();
 	fin.open (file);
 	if ( fin.is_open () ) {
-		cout << "\nOto nowa zawartosc pliku "
+		std::cout << "\nOto nowa zawartosc pliku "
 			<< file << ":\n";
 		while ( fin.get (ch) )
-			cout << ch;
+			std::cout << ch;
 		fin.close ();
 	}
 
 	fout.close ();
-	cout << "\nGotowe.\n";
+	std::cout << "\nGotowe.\n";
 	return 0;
 }
-
-
-
-	//cout << "Podaj imiona i nazwiska gosci(aby, zakonczyc, wprowadz pusty wiersz):\n";
-	//string name;
-	//while ( getline (cin, name) && name.size () > 0 ) {
-	//	fout << name << endl;
-	//}
-	//fout.close ();
